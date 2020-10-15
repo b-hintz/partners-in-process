ebf960401efdd6ee0fe3422316e757061f1520db
b-hintz <brandenhintz@gmail.com>
2020-10-15 15:48:39 -0500

    Updates

---
+++ precache-manifest.abfa072680c924a0c2cbccfd632aeaa4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7afd8445f310625ecb823da5b1ad022d",
+    "url": "/partners-in-process/index.html"
+  },
+  {
+    "revision": "247e1c5b2c38aef8b6e5",
+    "url": "/partners-in-process/static/css/main.e2d6064d.chunk.css"
+  },
+  {
+    "revision": "0634de0f2af2c57bebf7",
+    "url": "/partners-in-process/static/js/2.0aad3f4c.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/partners-in-process/static/js/2.0aad3f4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "247e1c5b2c38aef8b6e5",
+    "url": "/partners-in-process/static/js/main.2001563d.chunk.js"
+  },
+  {
+    "revision": "969303bed13372e16a4c",
+    "url": "/partners-in-process/static/js/runtime-main.b62e2fa2.js"
+  },
+  {
+    "revision": "b2dc0fa5fb1605fd322d4a5bc232a763",
+    "url": "/partners-in-process/static/media/email.b2dc0fa5.cjs"
+  }
+]);
